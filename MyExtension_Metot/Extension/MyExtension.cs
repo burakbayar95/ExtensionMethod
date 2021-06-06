8dfa5433bdd7423222c805f50dae03fa65337c4b
Burak <95burakbayar@gmail.com>
2021-06-06 18:04:55 +0300

    Commit

---
+++ MyExtension_Metot/Extension/MyExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+
+namespace MyExtension_Metot
+{
+   public static class MyExtension
+    {
+        public static int HarfSayisi(this string yazi)
+        {
+
+            string BosluksuzYazi = yazi.Replace(" ", "");
+
+            
+           
+
+
+           
+
+            return BosluksuzYazi.Length;
+
+        }
+
+
+    }
+}
---
+++ MyExtension_Metot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using MyExtension_Metot;
+
+using System.Linq;
+
+namespace MyExtension_Metot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Uzunluğunu bulmak istediğiniz metni giriniz: ");
+            string metin =Console.ReadLine();
+          
+            
+            Console.WriteLine("Metindeki harf sayisi: " + metin.HarfSayisi());
+           
+
+        
+
+          
+
+
+            
+            
+
+
+
+
+
+        }
+    }
+}
